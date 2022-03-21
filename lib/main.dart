260c5463453be033f30867e530f6f8bc19154c53
Bilalhassan679 <ftk4321@gmail.com>
2022-03-21 17:08:07 +0500

    Third Commit completed pickMultipleImage Work done in areaForm page

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:barat/screens/areaForm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screens/areaForm.dart
+++ lib/screens/areaForm.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:barat/widgets/reusableBigText.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../utils/color.dart';
 import '../widgets/reusableTextField.dart';
@@ -15,14 +19,19 @@ class AdminAreaForm extends StatefulWidget {
 }
 
 class _AdminAreaFormState extends State<AdminAreaForm> {
+  final ImagePicker _imagePicker = ImagePicker();
+  List<XFile> _selectedFiles = [];
+  FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+  List<String> arrimgsUrl = [];
+  int uploadItem = 0;
+  bool _upLoading = false;
+
   final TextEditingController areaName = TextEditingController();
-  final TextEditingController areaImage = TextEditingController();
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     areaName.dispose();
-    areaImage.dispose();
   }
 
   @override
@@ -60,17 +69,83 @@ class _AdminAreaFormState extends State<AdminAreaForm> {
                       SizedBox(
                         height: height * 0.01,
                       ),
+                      Expanded(
+                        child: _upLoading
+                            ? showLoading()
+                            : Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Center(
+                                    child: OutlinedButton(
+                                        onPressed: () {
+                                          selectImage();
+                                        },
+                                        child: Text('Select Files')),
+                                  ),
+                                  // Center(
+                                  //   child: ElevatedButton.icon(
+                                  //       onPressed: () {
+                                  //         if (_selectedFiles.isNotEmpty) {
+                                  //           uploadFunction(_selectedFiles);
+                                  //         } else {
+                                  //           ScaffoldMessenger.of(context)
+                                  //               .showSnackBar(SnackBar(
+                                  //                   content: Text(
+                                  //                       "PLEASE Select Image")));
+                                  //         }
+                                  //       },
+                                  //       icon: Icon(Icons.upload_sharp),
+                                  //       label: Text('Upload')),
+                                  // ),
+                                  Center(
+                                    child: _selectedFiles.length == null
+                                        ? Text("No Images Selected")
+                                        : Text(
+                                            'Image is Selected : ${_selectedFiles.length.toString()}'),
+                                  ),
 
+                                  Expanded(
+                                    child: GridView.builder(
+                                        itemCount: _selectedFiles.length,
+                                        gridDelegate:
+                                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                                crossAxisCount: 3),
+                                        itemBuilder:
+                                            (BuildContext context, int index) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(3.0),
+                                            child: Image.file(
+                                                File(
+                                                    _selectedFiles[index].path),
+                                                fit: BoxFit.cover),
+                                          );
+                                        }),
+                                  )
+                                ],
+                              ),
+                      ),
                       SizedBox(
                         height: height * 0.02,
                       ),
                       InkWell(
-                        onTap: () {
-                          Get.off(() => const AdminPage());
+                        onTap: () async {
+                          if (_selectedFiles.isNotEmpty) {
+                            uploadFunction(_selectedFiles);
+                            Get.to(AdminPage());
+                          } else if (areaName.toString().isNotEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text("PLEASE Type Area Name")));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("PLEASE Select Image")));
+                          }
                         },
                         child: const ReusableTextIconButton(
                           text: "Submit",
                         ),
+                      ),
+                      SizedBox(
+                        height: height * 0.15,
                       ),
                     ],
                   )),
@@ -80,4 +155,78 @@ class _AdminAreaFormState extends State<AdminAreaForm> {
       ),
     );
   }
+
+  //showLoading Login()
+  Widget showLoading() {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            "Uploading : " +
+                uploadItem.toString() +
+                "/" +
+                _selectedFiles.length.toString(),
+          ),
+          SizedBox(height: 30),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+  }
+  //Finish showLoading Login()
+
+  //upload ImageFile One by one
+  void uploadFunction(List<XFile> _images) async {
+    for (int i = 0; i < _images.length; i++) {
+      var imageUrl = await uploadFile(_images[i]);
+      arrimgsUrl.add(imageUrl.toString());
+    }
+    print("93 ${arrimgsUrl}");
+  }
+  //Finish upload ImageFile One by one
+
+  //Upload Images in Firestore Storage
+  Future<String> uploadFile(XFile _image) async {
+    setState(() {
+      _upLoading = true;
+    });
+    Reference reference =
+        _firebaseStorage.ref().child("Area images").child(_image.name);
+    await reference.putFile(File(_image.path)).whenComplete(() async {
+      setState(() {
+        uploadItem += 1;
+        if (uploadItem == _selectedFiles.length) {
+          _upLoading = false;
+          uploadItem = 0;
+        }
+      });
+    });
+    // await reference.getDownloadURL();
+    // print("111 ${await reference.getDownloadURL()}");
+    var img_url = await reference.getDownloadURL();
+    print('function print ${img_url}');
+    return img_url;
+    // return await reference.getDownloadURL();
+  }
+  //Finish Upload Images in Firestore Storage
+
+//Select Image From Gallery
+  Future<void> selectImage() async {
+    if (_selectedFiles != null) {
+      _selectedFiles.clear();
+    }
+
+    try {
+      final List<XFile>? imgs = await _imagePicker.pickMultiImage(
+          imageQuality: 50, maxWidth: 400, maxHeight: 400);
+      if (imgs!.isNotEmpty) {
+        _selectedFiles.addAll(imgs);
+      }
+      print("List of Images : " + imgs.length.toString());
+    } catch (e) {
+      print("Something Wrong" + e.toString());
+    }
+    setState(() {});
+  }
+//Finish Select Image From Gallery
 }
